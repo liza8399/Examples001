daa7cc423497a0f791e0ee38e72058ffa4eea26d
Lizaveta <obnimashka007@gmail.com>
2022-09-27 23:33:14 +0300

    Task 12

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,51 @@
+﻿// Вид 1  - ничего не возвращают, ничего не принимают
+void Method1()
+{
+    Console.WriteLine("Автор Елизавета");
+}
+// Method1();
+
+// Вид 2 - ничего не возвращают, но могут принимать какие-то аргументы
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+
+}
+//Method2(msg: "Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++; //инкремент
+    }
+}
+//Method21("Текст", 4);
+//Method21(count: 4, msg: "новый текст");
+
+//Вид 3 - ничего не принимают, но что-то возвращают
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+//Console.WriteLine(year);
+
+//Вид 4 - что-то принимают и что-то возвращают
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "asdf ");
+Console.WriteLine(res);
