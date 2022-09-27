e73599f297c78bcdea1f6ce0b751d9da4e6a3d51
Lizaveta <obnimashka007@gmail.com>
2022-09-28 00:17:13 +0300

    Add Example with for

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -46,4 +46,84 @@ string Method4(int count, string text)
 }
 
 string res = Method4(10, "asdf ");
-Console.WriteLine(res);
+//Console.WriteLine(res);
+
+
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+    //    Console.WriteLine($"{i} * {j} = {i * j}"); // интерпаляция строк
+    }
+    //Console.WriteLine();
+}
+
+//Дан текст. В тексте нужно все пробелы заменить черточками,
+//маленькие буквы "к" заменить бульшими "К",
+//а большие "С" заменить маленькими "с".
+
+string text = "- Я думаю, - сказал князь, улыбаясь, что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+// string s = "qwerty"
+//             012
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+       if(text [i] == oldValue) result = result + $"{newValue}";
+       else result = result + $"{text[i]}"; 
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '_');
+
+//Console.WriteLine(newText);
+//Console.WriteLine();
+//newText = Replace(newText, 'к', 'К');
+//Console.WriteLine(newText);
+
+//Console.WriteLine();
+//newText = Replace(newText, 'С', 'с');
+//Console.WriteLine(newText);
+
+int[] arr = {1, 5, 7, 4, 1, 6, 3, 2, 1};
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int minPosition = i;
+        
+        for (int j = i + 1; j < array.Length; j++) // почему указал здесь -1
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
